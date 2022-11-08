040d93d511518ac501e78c2724765aea74418536
oldri kecaj <oldrikecaj@hotmail.com>
2022-11-08 14:05:17 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    let bpm = document.getElementById('bpm').value;
+    let semitone = document.getElementById('semitone').value;
+    
+   if(isNaN(bpm) || !bpm || isNaN(semitone) || !semitone){
+        alert("Must enter a valid number slatt.");
+   }else{
+        let newSemi = parseFloat(semitone / 12);
+        let power = Math.pow(2, newSemi);
+        let newBPM = parseFloat(bpm * power);
+        let bpmHeader = document.createElement('h2');
+        bpmHeader.innerHTML = newBPM;
+        document.getElementById('form').appendChild(bpmHeader);
+   }
+}
